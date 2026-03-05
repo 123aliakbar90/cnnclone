bf6782c7b81a12626b717e41c5995042480d37eb
123aliakbar90 <123aliakbar90@gmail.com>
2026-03-05 12:43:47 +0500

    Add database connection setup in db.php

---
+++ db.php
@@ -0,0 +1,18 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$host = 'localhost';
+$dbname = 'rsk36_rsk36_1';
+$user = 'rsk36_rsk36_1';
+$pass = '654321#';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    die("Database Connection Failed: " . $e->getMessage());
+}
+?>
